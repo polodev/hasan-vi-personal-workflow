70eeee6c32521065aaccf3270fbf7c053b2e32be
Hasan <abulhasan408@gmail.com>
2022-04-16 14:22:46 +0600

    service added

---
+++ vendor/main.js
@@ -0,0 +1,35 @@
+(function ($) {
+    "use strict";
+    
+    /*-------------------------------------
+    Isotope
+    -------------------------------------*/
+    $('.isotope-wrapper').imagesLoaded(function () {
+        // filter items on button click
+        $('.isotope-button').on('click', 'button', function () {
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({
+                filter: filterValue
+            });
+        });
+        
+        // init Isotope
+        var $grid = $('.isotope-list').isotope({
+            itemSelector: '.services',
+            percentPosition: true,
+            transitionDuration: '0.7s',
+            layoutMode: 'fitRows',
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: 1,
+            }
+        });
+    });
+
+    $('.isotope-button button').on('click', function (event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+   
+})(jQuery);
